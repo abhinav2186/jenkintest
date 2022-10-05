7512ebabb8f1785ad4a24ed6377b330d880f6d0d
elucidate <abhinav2186@gmail.com>
2022-10-05 21:42:56 +0530

    first code

---
+++ src/test/java/com/jenky/TestClass1.java
@@ -0,0 +1,39 @@
+package com.jenky;
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+
+public class TestClass1 {
+
+
+	@Test
+	public void testMethod1()
+	{
+		System.out.println("Inside test method 1 "+Thread.currentThread().getId());
+		
+	}
+	
+	@Test
+	public void testMethod2()
+	{
+		System.out.println("Inside test method 2 "+Thread.currentThread().getId());
+		Assert.fail();
+	}
+	
+	@Test(dependsOnMethods = "testMethod2")
+	public void testMethod3()
+	{
+		System.out.println("Inside test method 3 "+Thread.currentThread().getId());
+		
+	}
+	
+	@Test
+	public void testMethod4()
+	{
+		System.out.println("Inside test method 4 "+Thread.currentThread().getId());
+	}
+	
+	
+}
