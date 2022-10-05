419162303a2cc0e6bbe4175087541f462b688e4d
elucidate <abhinav2186@gmail.com>
2022-10-05 22:41:53 +0530

    my second commit

--- src/test/java/com/jenky/TestClass1.java
+++ src/test/java/com/jenky/TestClass1.java
@@ -11,28 +11,28 @@ public class TestClass1 {
 	@Test
 	public void testMethod1()
 	{
-		System.out.println("Inside test method 1 "+Thread.currentThread().getId());
+		System.out.println("Inside test method 1 and in TestClass1 "+Thread.currentThread().getId());
 		
 	}
 	
 	@Test
 	public void testMethod2()
 	{
-		System.out.println("Inside test method 2 "+Thread.currentThread().getId());
+		System.out.println("Inside test method 2  and in TestClass1"+Thread.currentThread().getId());
 		Assert.fail();
 	}
 	
 	@Test(dependsOnMethods = "testMethod2")
 	public void testMethod3()
 	{
-		System.out.println("Inside test method 3 "+Thread.currentThread().getId());
+		System.out.println("Inside test method 3  and in TestClass1  "+Thread.currentThread().getId());
 		
 	}
 	
 	@Test
 	public void testMethod4()
 	{
-		System.out.println("Inside test method 4 "+Thread.currentThread().getId());
+		System.out.println("Inside test method 4  and in TestClass1 "+Thread.currentThread().getId());
 	}
 	
 	
